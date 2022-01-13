38f60cd191aa0e06f94cca541b2eccb741d874d2
sirbhuan <97625843+sirbhuan@users.noreply.github.com>
2022-01-13 15:05:16 -0500

    Update App.java
    change data

--- src/main/java/Aaditya/SpringAutoWriteExample/App.java
+++ src/main/java/Aaditya/SpringAutoWriteExample/App.java
@@ -3,7 +3,7 @@ package Aaditya.SpringAutoWriteExample;
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("Hello Java");
   }
 
 }
