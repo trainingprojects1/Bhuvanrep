b5dab9e33d68235c44914d2b857caeee055a2240
Bhuvan Adhikari <bhuwanjhapali@gmial.com>
2022-01-13 15:21:29 -0500

    second commit

--- src/main/java/Aaditya/SpringAutoWriteExample/App.java
+++ src/main/java/Aaditya/SpringAutoWriteExample/App.java
@@ -3,7 +3,7 @@ package Aaditya.SpringAutoWriteExample;
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("Hello Java");
+    System.out.println("Hello Java change");
   }
 
 }
