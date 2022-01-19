ad8fd50d4c07e832294760b33d4daf991d5ed887
sirbhuan <97625843+sirbhuan@users.noreply.github.com>
2022-01-18 22:35:45 -0500

    Update Demo1.java
    remove ,,,,,

--- src/main/java/com/java/test/practices1/Demo1.java
+++ src/main/java/com/java/test/practices1/Demo1.java
@@ -8,7 +8,7 @@ public class Demo1 {
 	}
 		public static void main(String[] args) {
 			Demo1 obj = new Demo1();
-			String name = obj.getFullName("Aadityaa--   ", "Adhikari");
+			String name = obj.getFullName("Aaditya  ", "Adhikari");
 			System.out.println(name);
 		}
 
