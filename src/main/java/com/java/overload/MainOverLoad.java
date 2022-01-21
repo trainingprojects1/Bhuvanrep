8496973056575cdd7789d660ffd07e7b9a452fe7
Bhuvan Adhikari <bhuwanjhapali@gmial.com>
2022-01-21 16:07:57 -0500

    Add Overloading

---
+++ src/main/java/com/java/overload/MainOverLoad.java
@@ -0,0 +1,13 @@
+package com.java.overload;
+
+public class MainOverLoad {
+
+	public static void main(String[] args) {
+		MattOverlaoding ma = new MattOverlaoding();
+		 double c =ma.divide(8, 3);
+		System.out.println(c);
+		ma.multiply(5, 2);
+		ma.sub(5, 6);
+	}
+
+}
---
+++ src/main/java/com/java/overload/MattOverlaoding.java
@@ -0,0 +1,18 @@
+package com.java.overload;
+
+public class MattOverlaoding {
+	
+	public double divide(int a, double b) {
+		double c= a/b;
+		return c;
+		
+	}
+public void multiply(int a, int b) {
+	int c = a+b;
+	System.out.println(c);
+}
+public void sub( int a , int b) {
+	int c = a-b;
+	System.out.println(c);
+}
+}
